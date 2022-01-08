f49dd2ce5c21876cd0462cd3b9095531e893738f
lincolnschick <lincolntschick@gmail.com>
2022-01-07 23:00:09 -0600

    Created English databases

--- to_ipa/ipa_converter/models.py
+++ to_ipa/ipa_converter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class NarrowIPA(models.Model):
+    word = models.CharField(max_length=200)
+    transcription1 = models.TextField()
+    transcription2 = models.TextField()
+    transcription3 = models.TextField()
+    transcription4 = models.TextField()
+
+class BroadIPA(models.Model):
+    word = models.CharField(max_length=200)
+    transcription1 = models.TextField()
+    transcription2 = models.TextField()
+    transcription3 = models.TextField()
+    transcription4 = models.TextField()
